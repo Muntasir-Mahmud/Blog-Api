b42024054870b5bffd272f7f9792ddf735e98b62
Muntasir-Mahmud <m.siyam529@gmail.com>
2020-07-22 20:11:41 +0600

    Add view level permission and login/logout

--- blog_api-project/blog_api/urls.py
+++ blog_api-project/blog_api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')), 
     #Here add v1 is a good practice, which means version:1
+    path('api-auth/',include('rest_framework.urls')),
+    #This url is for the auth of the api 
 ]

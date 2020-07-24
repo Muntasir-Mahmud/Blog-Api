40abb12b246ef695e45b856efd85c5d0b87746ac
Muntasir-Mahmud <m.siyam529@gmail.com>
2020-07-24 17:59:05 +0600

    Add ViewSet instead of GenericViews and usse routers

--- blog_api-project/posts/serializers.py
+++ blog_api-project/posts/serializers.py
@@ -1,7 +1,15 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from . models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = '__all__'
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
